703bfd3c22fe5120305c26353ef82bed63715945
ltanme <1917130+ltanme@users.noreply.github.com>
2021-07-26 17:27:43 +0800

    Update wxwork.py

--- wxwork.py
+++ wxwork.py
@@ -7,11 +7,14 @@ import sys
 import requests
 import os
 reload(sys)
+import datetime
+
 sys.setdefaultencoding('utf8')
 
 if __name__ == '__main__':
     url = os.environ["DA_NAO_WX_ROBOT"]
-    payload = '{"msgtype": "text","text": {"content": "您今天打卡了吗？我是来自github action 机器人","mentioned_list":["@all"],"mentioned_mobile_list":["@all"]}}'
+    now = datetime.datetime.now()
+    payload = '{"msgtype": "text","text": {"content": "您今天打卡了吗？我是来自github action 机器人,当前时间:%s","mentioned_list":["@all"],"mentioned_mobile_list":["@all"]}}' % now
     headers = {
         'Content-Type': "application/json",
     }
